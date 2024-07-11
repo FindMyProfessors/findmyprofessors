82cc6be0d4f2919d0a383eec8b591e68d59af636
Jandro-WDS <jandroschool@gmail.com>
2024-07-10 23:29:58 -0400

    professor cards updated design

--- mobile/app/lib/screens/dashboard.dart
+++ mobile/app/lib/screens/dashboard.dart
@@ -2,6 +2,7 @@
 import 'package:app/screens/Professor.dart';
 import 'package:app/widgets/sideMenu.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({ Key? key }) : super(key: key);
@@ -261,16 +262,67 @@ void _showFilter() {
             Navigator.of(context).push(MaterialPageRoute(builder: (context) => Professor(item: "${index + 1}", rating: (index+1)%5,)));
           },
           child: Container(
+            height: 100.0,
             margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
             padding: EdgeInsets.all(16.0),
             decoration: BoxDecoration(
               color: Color(0xff17203a),
               borderRadius: BorderRadius.circular(10.0),
             ),
-            child: Text(
-              professors[index],
-              style: TextStyle(color: Colors.white, fontSize: 18.0),
+
+            child: Column(
+              
+              children: [
+                SizedBox(height: 5.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      professors[index],
+                      style: TextStyle(color: Colors.white, fontSize: 18.0),
+                    ),
+
+                    RatingBar.builder(
+                      ignoreGestures: true,
+                      itemSize: 20.0,
+                      initialRating: index.toDouble()%5+1,
+                      minRating: 0,
+                      direction: Axis.horizontal,
+                      //allowHalfRating: true,
+                      itemCount: 5,
+                      itemPadding: EdgeInsets.symmetric(horizontal: 0.0),
+                      itemBuilder: (context, _) => Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      onRatingUpdate: (rating) {
+                        print(rating);
+                      },
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 10.0),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                  Text(
+                  "Course Name "+index.toString(),
+                  style: TextStyle(color: Colors.white, fontSize: 12.0),
+                  )
+                  ]
+
+                ),
+      
+              ]
+              
+
             ),
+            
+            
+            
+            
           )
         );
       },
