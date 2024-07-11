075ac1abec287c64464a35a5f50e826775afe03d
Jandro-WDS <jandroschool@gmail.com>
2024-07-11 00:15:17 -0400

    table + scroll for professors screen

--- mobile/app/lib/screens/Professor.dart
+++ mobile/app/lib/screens/Professor.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
@@ -60,101 +61,151 @@ class Professor extends StatelessWidget {
                 ),
               ],
             ),
-
-            SizedBox(height: 40.0),
-
-            Text(
-              'Parameters: ',
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-            ),
-
-            SizedBox(height: 20.0),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  height: 250,
-                  width: 250,
-                  child: 
-                    RadarChart(
-                      RadarChartData(
-                        titlePositionPercentageOffset: 0.2,       // Set the distance between the graph edge and the titles text
-                        radarBackgroundColor: Colors.transparent, // Set the background color of the graph to be transparent
-                        radarShape: RadarShape.polygon,           // Set the shape of the graph to be a polygon (instead of the default circle)
-                        radarBorderData: const BorderSide(
-                          color: Colors.grey,   // Set the color of outermost tick line
-                          width: 0.5,           // Set the width of outermost tick line
-                        ),
-                        tickCount: 5,           // Set the number of tick lines that go around the center
-                        tickBorderData: const BorderSide(
-                          color: Colors.grey,   // Set the color of inside tick lines that go around the center
-                          width: 0.5,           // Set the width of inside tick lines that go around the center
-                        ),
-                        ticksTextStyle: const TextStyle(
-                          color: Colors.black,         // Set the color of the tick text (numbers shown inside the chart)
-                          fontWeight: FontWeight.w500, // Set the font weight of the tick text (numbers shown inside the chart)
-                          fontSize: 9,                 // Set the font size of the tick text (numbers shown inside the chart)
-                        ),
-                        gridBorderData: const BorderSide(
-                          color: Colors.grey, // Set the color of the grid lines that expand out from the center
-                          width: 1,           // Set the width of the grid lines that expand out from the center
-                        ),
-                        titleTextStyle: const TextStyle(
-                          color: Colors.black,         // Set the color of the title text that goes around the chart
-                          fontWeight: FontWeight.w500, // Set the font weight of the title text that goes around the chart
-                          fontSize: 11,                // Set the font size of the title text that goes around the chart
-                        ),
-
-                        getTitle: (index, angle) {
-                          final usedAngle = angle;
-                              return RadarChartTitle(
-                                text: 'Parameter ' + index.toString(),
-                                angle: usedAngle,
-                                positionPercentageOffset: .05);
-                        },
-                        
-                        dataSets: [
-                          RadarDataSet(
-                            fillColor: Colors.black.withOpacity(0.3),
-                            borderColor: Colors.black,
-                            borderWidth: 2,
-                            entryRadius: 5,
-                            dataEntries: [
-                              RadarEntry(value: random.nextDouble() * 5),
-                              RadarEntry(value: random.nextDouble() * 5),
-                              RadarEntry(value: random.nextDouble() * 5),
-                              RadarEntry(value: random.nextDouble() * 5),
-                              RadarEntry(value: random.nextDouble() * 5),
-                              RadarEntry(value: random.nextDouble() * 5),
-                              RadarEntry(value: random.nextDouble() * 5),
-                              RadarEntry(value: random.nextDouble() * 5),
-                            ],
+            
+            Expanded(
+              child: ListView.builder(
+                itemCount: 1, // Replace with your actual item count
+                itemBuilder: (BuildContext context, int index) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 40.0),
+                      Text(
+                        'Parameters: ',
+                        style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 20.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            alignment: Alignment.center,
+                            height: 250,
+                            width: 250,
+                            child: RadarChart(
+                              RadarChartData(
+                                titlePositionPercentageOffset: 0.2,
+                                radarBackgroundColor: Colors.transparent,
+                                radarShape: RadarShape.polygon,
+                                radarBorderData: BorderSide(
+                                  color: Colors.grey,
+                                  width: 0.5,
+                                ),
+                                tickCount: 5,
+                                tickBorderData: BorderSide(
+                                  color: Colors.grey,
+                                  width: 0.5,
+                                ),
+                                ticksTextStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 9,
+                                ),
+                                gridBorderData: BorderSide(
+                                  color: Colors.grey,
+                                  width: 1,
+                                ),
+                                titleTextStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 11,
+                                ),
+                                getTitle: (index, angle) {
+                                  final usedAngle = angle;
+                                  return RadarChartTitle(
+                                    text: 'Parameter ' + index.toString(),
+                                    angle: usedAngle,
+                                    positionPercentageOffset: .05,
+                                  );
+                                },
+                                dataSets: [
+                                  RadarDataSet(
+                                    fillColor: Colors.black.withOpacity(0.3),
+                                    borderColor: Colors.black,
+                                    borderWidth: 2,
+                                    entryRadius: 5,
+                                    dataEntries: [
+                                      RadarEntry(value: random.nextDouble() * 5),
+                                      RadarEntry(value: random.nextDouble() * 5),
+                                      RadarEntry(value: random.nextDouble() * 5),
+                                      RadarEntry(value: random.nextDouble() * 5),
+                                      RadarEntry(value: random.nextDouble() * 5),
+                                      RadarEntry(value: random.nextDouble() * 5),
+                                      RadarEntry(value: random.nextDouble() * 5),
+                                      RadarEntry(value: random.nextDouble() * 5),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
                           ),
                         ],
-                        
                       ),
-                      //swapAnimationDuration: Duration(milliseconds: 10), // Optional
-                      //swapAnimationCurve: Curves.linear, // Optional
-                    ),
-                  ),
-              ],
+                      SizedBox(height: 20.0),
+
+                      Text(
+                        'This is some additional information about Item $index',
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+
+                      SizedBox(height: 20.0),
+
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                          )
+                        ),
+                        height: 250,
+                        width: double.infinity,
+                        child: DataTable2(
+                          columnSpacing: 12,
+                          horizontalMargin: 12,
+                          minWidth: 600,
+                          columns: [
+                            DataColumn2(
+                              label: Text('Column A'),
+                              size: ColumnSize.L,
+                            ),
+                            DataColumn(
+                              label: Text('Column B'),
+                            ),
+                            DataColumn(
+                              label: Text('Column C'),
+                            ),
+                            DataColumn(
+                              label: Text('Column D'),
+                            ),
+                            DataColumn(
+                              label: Text('Column NUMBERS'),
+                              numeric: true,
+                            ),
+                          ],
+                          rows: List<DataRow>.generate(
+                            100,
+                            (index) => DataRow(cells: [
+                              DataCell(Text('A' * (10 - index % 10))),
+                              DataCell(Text('B' * (10 - (index + 5) % 10))),
+                              DataCell(Text('C' * (15 - (index + 5) % 10))),
+                              DataCell(Text('D' * (15 - (index + 10) % 10))),
+                              DataCell(Text(((index + 0.1) * 25.4).toString())),
+                            ]),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('Add to Course Cart'),
+                      ),
+                    ],
+                  );
+                },
+              )
             ),
-            
-            
-            SizedBox(height: 20.0),
-            Text(
-              'This is some additional information about $item',
-              style: TextStyle(fontSize: 16.0),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Add to Course Cart'),
-            ),
+          
           ],
         ),
       ),
