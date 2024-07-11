042a087c3f5fbd06921e47254108fe7b2e29a9c5
Zachary Cary <zachcary1@gmail.com>
2024-07-11 01:04:35 +0000

    register with backend but has error with ports

--- website2/src/components/Register.jsx
+++ website2/src/components/Register.jsx
@@ -1,27 +1,97 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
 import Header from './Header';
 import {
   MDBBtn,
   MDBContainer,
   MDBCard,
   MDBCardBody,
-  MDBCardImage,
   MDBRow,
   MDBCol,
   MDBIcon,
   MDBInput,
-  MDBNavbar,
-  MDBNavbarBrand,
-  MDBNavbarNav,
-  MDBNavbarItem,
-  MDBNavbarLink,
-  MDBCollapse,
 } from 'mdb-react-ui-kit';
 
-
-
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  }
+
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError('');
+    setSuccess('');
+
+    if (!formData.username) {
+      setError('Username is required');
+      return;
+    }
+
+    if (!validateEmail(formData.email)) {
+      setError('Invalid email address');
+      return;
+    }
+
+    if (!formData.password) {
+      setError('Password is required');
+      return;
+    }
+
+    if (formData.password !== formData.confirmPassword) {
+      setError('Passwords do not match');
+      return;
+    }
+
+    const { email, username, password } = formData;
+
+    try {
+      console.log('Sending request with data:', { email, username, password }); // Debugging log
+      const response = await fetch('http://localhost:8080/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, username, password })
+      });
+
+      if (response.ok) {
+        setSuccess('Registration successful! Redirecting to login...');
+        setError('');
+        console.log('Registration successful'); // Debugging log
+        setTimeout(() => {
+          navigate('/Login'); // Redirect to login page after successful registration
+        }, 2000); // Wait for 2 seconds before redirecting
+      } else {
+        const errorMessage = await response.text();
+        setError(errorMessage);
+        console.log('Registration failed:', errorMessage); // Debugging log
+      }
+    } catch (error) {
+      setError('An error occurred');
+      console.log('An error occurred:', error); // Debugging log
+    }
+  }
 
   const containerStyle = {
     maxWidth: '500px',
@@ -30,36 +100,39 @@ const Register = () => {
   }
 
   return (
-    <>  
-        <Header />
-
-        <MDBContainer style={containerStyle}>
-          <MDBCard style={{ maxWidth: '500px', margin: '0 auto' }}>
-            <MDBRow className='g-0'>
-              <MDBCol md='0' style={{ backgroundColor: 'black' }}></MDBCol>
-              <MDBCol md='12'>
-                <MDBCardBody className='d-flex flex-column'>
-                  <div className='d-flex flex-row mt-2'>
-                    <MDBIcon icon="search fa-3x me-3" style={{ color: '#000000' }}/>
-                    <span className="h1 fw-bold mb-0">Find My Professors</span>
-                  </div>
-                  <h5 className="fw-normal my-4 pb-3" style={{letterSpacing: '1px'}}>Register Now</h5>
-                  <MDBInput wrapperClass='mb-4' label='Username' id='formControlLg' size="lg"/>
-                  <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
-                  <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
-                  <MDBInput wrapperClass='mb-4' label='Confirm Password' id='formControlLg' type='password' size="lg"/>
-                  <MDBBtn className="mb-4 px-5" color='dark' size='lg'>Register</MDBBtn>
-                  <p className="mb-5 pb-lg-2" style={{color: '#393f81'}}>Already have an account? <Link to="/Login">Login</Link></p>
-                  <div className='d-flex flex-row justify-content-start'>
-                    <a href="#!" className="small text-muted me-1">Terms of use.</a>
-                    <a href="#!" className="small text-muted">Privacy policy</a>
-                  </div>
-                </MDBCardBody>
-              </MDBCol>
-            </MDBRow>
-          </MDBCard>
-        </MDBContainer>
-      </>
+    <>
+      <Header />
+      <MDBContainer style={containerStyle}>
+        <MDBCard style={{ maxWidth: '500px', margin: '0 auto' }}>
+          <MDBRow className='g-0'>
+            <MDBCol md='0' style={{ backgroundColor: 'black' }}></MDBCol>
+            <MDBCol md='12'>
+              <MDBCardBody className='d-flex flex-column'>
+                <div className='d-flex flex-row mt-2'>
+                  <MDBIcon icon="search fa-3x me-3" style={{ color: '#000000' }} />
+                  <span className="h1 fw-bold mb-0">Find My Professors</span>
+                </div>
+                <h5 className="fw-normal my-4 pb-3" style={{ letterSpacing: '1px' }}>Register Now</h5>
+                <form onSubmit={handleSubmit}>
+                  <MDBInput wrapperClass='mb-4' label='Username' id='formControlLg' size="lg" name="username" onChange={handleChange} value={formData.username} />
+                  <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg" name="email" onChange={handleChange} value={formData.email} />
+                  <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg" name="password" onChange={handleChange} value={formData.password} />
+                  <MDBInput wrapperClass='mb-4' label='Confirm Password' id='formControlLg' type='password' size="lg" name="confirmPassword" onChange={handleChange} value={formData.confirmPassword} />
+                  {error && <p style={{ color: 'red' }}>{error}</p>}
+                  {success && <p style={{ color: 'green' }}>{success}</p>}
+                  <MDBBtn type="submit" className="mb-4 px-5" color='dark' size='lg'>Register</MDBBtn>
+                </form>
+                <p className="mb-5 pb-lg-2" style={{ color: '#393f81' }}>Already have an account? <Link to="/Login">Login</Link></p>
+                <div className='d-flex flex-row justify-content-start'>
+                  <a href="#!" className="small text-muted me-1">Terms of use.</a>
+                  <a href="#!" className="small text-muted">Privacy policy</a>
+                </div>
+              </MDBCardBody>
+            </MDBCol>
+          </MDBRow>
+        </MDBCard>
+      </MDBContainer>
+    </>
   );
 }
 
