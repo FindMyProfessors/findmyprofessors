2dcab63c020e01d825ab642e8cd6ffcdb1cbb13a
Zachary Cary <zachcary1@gmail.com>
2024-07-22 13:04:40 -0400

    formatting

--- website2/src/components/Home.jsx
+++ website2/src/components/Home.jsx
@@ -32,25 +32,55 @@ const Home = () => {
 
         <style>{'body { background-color: #121212; }'}</style>
 
-        <MDBContainer style={containerStyle}>
-          <MDBCard style={{ maxWidth: '1800px', margin: '0 auto', backgroundColor: '#282828', boxShadow: '3px 3px 12px rgba(0, 0, 0, 0.75)' }}>
-            <MDBRow className='g-0'>
-              <MDBCol md='0' style={{ backgroundColor: 'black' }}></MDBCol>
-              <MDBCol md='12'>
-                <MDBCardBody className='d-flex flex-column'>
+        <MDBContainer
+        style={containerStyle}>
 
-                  <div className='d-flex flex-row mt-2 justify-content-center'>
-                    <MDBIcon icon="search fa-4x me-4" style={{ color: 'white' }}/>
-                    <span className="h1 fw-bold mb-4" style={{ fontSize: '4rem', color:'white' }}>Find My Professors</span>
+          <MDBCard
+          style={{ maxWidth: '1800px', margin: '0 auto', backgroundColor: '#282828', boxShadow: '3px 3px 12px rgba(0, 0, 0, 0.75)' }}>
+
+            <MDBRow
+            className='g-0'>
+
+              <MDBCol
+              md='0'
+              style={{ backgroundColor: 'black' }}>
+              </MDBCol>
+
+              <MDBCol
+              md='12'>
+
+                <MDBCardBody
+                className='d-flex flex-column'>
+
+                  <div
+                  className='d-flex flex-row mt-2 justify-content-center'>
+
+                    <MDBIcon
+                    icon="search fa-4x me-4"
+                    style={{ color: 'white' }}/>
+
+                    <span
+                    className="h1 fw-bold mb-4"
+                    style={{ fontSize: '4rem', color:'white' }}>
+                      Find My Professors
+                    </span>
+
                   </div>
 
-                  <h5 className="fw-normal my-3 pb-0 text-center" style={{letterSpacing: '1px', color: '#DDDDDD'}}>
-                    No more copy-pasting names into RateMyProfessors</h5>
+                  <h5
+                  className="fw-normal my-3 pb-0 text-center"
+                  style={{letterSpacing: '1px', color: '#DDDDDD'}}>
+                    No more copy-pasting names into RateMyProfessors
+                  </h5>
 
-                  <h5 className="fw-normal pb-5 text-center" style={{letterSpacing: '1px', color: '#DDDDDD'}}>
-                    Use Find My Professors to find the best professors for you</h5>
+                  <h5
+                  className="fw-normal pb-5 text-center"
+                  style={{letterSpacing: '1px', color: '#DDDDDD'}}>
+                    Use Find My Professors to find the best professors for you
+                  </h5>
 
-                  <div className="d-flex justify-content-center">
+                  <div
+                  className="d-flex justify-content-center">
 
                       <MDBBtn
                       color="primary"
@@ -70,14 +100,11 @@ const Home = () => {
                       </MDBBtn>
 
                   </div>
-
                 </MDBCardBody>
               </MDBCol>
             </MDBRow>
           </MDBCard>
         </MDBContainer>
-        
-        
     </>
     );
 };
